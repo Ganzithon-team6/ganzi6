c99f0a4a4ab1757ad8dd24d59da4f65ffc997ef9
KyungHoon03 <tw21lkh@naver.com>
2025-11-15 11:10:28 +0900

    [set]

---
+++ src/main/java/com/example/ganzi6/Ganzi6Application.java
@@ -0,0 +1,13 @@
+package com.example.ganzi6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ganzi6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ganzi6Application.class, args);
+    }
+
+}
